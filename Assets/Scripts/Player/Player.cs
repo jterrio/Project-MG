9ad5faa88a736156adab412e4eca577cb3e99bbf
jterrio <31709118+jterrio@users.noreply.github.com>
2020-07-25 16:51:41 -0500

    Added AmmoCount and Player script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public TMPro.TextMeshProUGUI ammoCountText;
+    public PlayerMovement pm;
+    public Gun gun;
+
+    void Start() {
+        gun = GetComponentInChildren<Gun>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        UpdateAmmoCount();
+    }
+
+
+    void UpdateAmmoCount() {
+        if (ammoCountText != null) {
+            if (gun.isReloading) {
+                ammoCountText.text =   "--/" + gun.magSize.ToString();
+            } else {
+                ammoCountText.text = gun.bulletsInMag.ToString() + "/" + gun.magSize.ToString();
+            }
+
+        }
+    }
+}
