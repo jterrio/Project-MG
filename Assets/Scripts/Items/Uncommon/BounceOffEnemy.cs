ca85729595b400bf78afc4b13b12ba13ba16244f
jterrio <31709118+jterrio@users.noreply.github.com>
2020-11-23 17:54:07 -0600

    Added BouncingOffEnemy PowerUp
    -Added Bouncing Off Enemy Power Up item at the Uncommon level
    -Additional stacks of bouncing off enemies increase number of bounces
    -Updated layering of Enemies such that only the part that contains the collider is in the layer of "Enemy"
    -Updated Bullet prefab to hold information, such as who it hit and number of bounces

---
+++ Assets/Scripts/Items/Uncommon/BounceOffEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceOffEnemy : WepItem {
+
+    public override void GetItemEffects() {
+        GameManager.gm.p.gun.timesToBounceOffEnemies += 1;
+        if (GameManager.gm.p.gun.timesToBounceOffEnemies == 1) {
+            ItemManager.im.bulletHitDelegate += ItemManager.im.BounceOffEnemy;
+        }
+    }
+
+}
--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -6,7 +6,9 @@ public class Bullet : MonoBehaviour
 {
 
     public float bulletLifetime;
+    public int numberOfBounces = 0;
     public GameObject hitEffect;
+    public GameObject monsterToIgnore;
 
     // Start is called before the first frame update
     void Start() {
@@ -21,22 +23,35 @@ public class Bullet : MonoBehaviour
         if(collision.gameObject.layer == LayerMask.NameToLayer("Bullet")) {
             return;
         }
+
         Collider myCollider = collision.contacts[0].otherCollider;
-        GameObject g = Instantiate(hitEffect, collision.contacts[0].point, Quaternion.LookRotation(collision.contacts[0].normal));
-        Destroy(g, 4f);
+ 
+
         if (myCollider.gameObject.layer == LayerMask.NameToLayer("Enemy")) {
             Enemy e = myCollider.gameObject.GetComponent<Enemy>();
             if(e != null) {
+                if (myCollider.gameObject == monsterToIgnore) {
+                    return;
+                }
                 Debug.Log("Hit: " + myCollider.gameObject.name + "... Health: " + e.health);
                 e.TakeDamage(GameManager.gm.p.gun.GetDamage());
+                ItemManager.im.bulletHitDelegate?.Invoke(this.gameObject, this, e.gameObject);
             } else {
+                if (myCollider.gameObject.transform.parent.gameObject == monsterToIgnore) {
+                    return;
+                }
                 e = myCollider.gameObject.GetComponentInParent<Enemy>();
                 if(e != null) {
                     Debug.Log("Hit: " + myCollider.gameObject.name + "... Health: " + e.health);
                     e.TakeDamage(GameManager.gm.p.gun.GetDamage());
+                    ItemManager.im.bulletHitDelegate?.Invoke(this.gameObject, this, e.gameObject);
                 }
             }
         }
+
+
+        GameObject g = Instantiate(hitEffect, collision.contacts[0].point, Quaternion.LookRotation(collision.contacts[0].normal));
+        Destroy(g, 4f);
         Destroy(this.gameObject);
     }
 
